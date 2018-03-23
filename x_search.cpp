ccfb7cdcd29cb760c5c6279e251270574945b3a8
79637706045 <36453460+79637706045@users.noreply.github.com>
2018-03-23 09:55:05 +0300

    Add files via upload

---
+++ x_search.cpp
@@ -0,0 +1,9 @@
+void x_search(yzel* tempr, int* counter){
+			if(tempr){
+				if(tempr->type == 2){
+					(*counter)++;
+				}
+				x_search(tempr->left, counter);
+				x_search(tempr->right, counter);
+			}
+	}
