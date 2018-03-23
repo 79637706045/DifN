abc2c894c4fc4e1d0e0f988266eb8406bd02f148
79637706045 <36453460+79637706045@users.noreply.github.com>
2018-03-23 11:31:45 +0300

    Add files via upload

--- x_search.cpp
+++ x_search.cpp
@@ -1,9 +1,9 @@
-void x_search(yzel* tempr, int* counter){
-			if(tempr){
-				if(tempr->type == 2){
-					(*counter)++;
-				}
-				x_search(tempr->left, counter);
-				x_search(tempr->right, counter);
-			}
+void sTree::x_search(yzel* tempr, int* counter){
+	if(tempr){
+		if(tempr->type == 2){
+			(*counter)++;
+		}
+	x_search(tempr->left, counter);
+	x_search(tempr->right, counter);
 	}
+}
